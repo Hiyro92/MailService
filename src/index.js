1c1c77f6c3cf5bae577dfb09e469cb028fd03e18
Hiyro92 <Hiyro92@gmail.com>
2022-02-25 12:25:02 +0100

    remove typo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 const app = require("./app");
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server listening on Port: ${port}`);
-});
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+
+const http_port = process.env.HTTP_PORT || 5000;
+const https_port = process.env.HTTPS_PORT || 5001;
+
+http
+  .createServer(app)
+  .listen(http_port, () =>
+    console.log(`HTTP-Server listening on Port: ${http_port}`)
+  );
+/* https
+  .createServer(
+    {
+      key: fs.readFileSync(path.join(__dirname, "/keys/server.key")),
+      cert: fs.readFileSync(path.join(__dirname, "/keys/server.cert")),
+    },
+    app
+  )
+  .listen(https_port, () =>
+    console.log(`HTTPS-Server listening on Port: ${https_port}`)
+  );
+ */
